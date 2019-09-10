32585d551e98abc886d2fe9758ee03f5320f5083
adityakhowala <aditya.khowala51295@gmail.com>
2019-09-10 17:49:58 +0530

    Adding what checklist to perform

---
+++ apache2-harden.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# https://geekflare.com/apache-web-server-hardening-security/
+# https://geekflare.com/10-best-practices-to-secure-and-harden-your-apache-web-server/
+# https://www.tecmint.com/apache-security-tips/
+
+
+echo " ------------------------------- "
+echo "     Apache2 Server Hardening    "
+echo " ------------------------------- "
+
+echo -e "\nPerforming These Checklist 
+1. hide Apache Version and OS Identity from Errors
+2. Disable Directory Listing    	
+3. Use only TLS 1.2, Disable SSLv2, SSLv3
+4. Disable Null and Weak Ciphers
+5. Disabling TRACE support 
+6. Disable Risky Http Methods
+7. Protect binary and configuration directory permission
+8. System Settings Protection
+9. Enable XSS Protection
+10. Disable HTTP 1.0 Protocol
+     "
